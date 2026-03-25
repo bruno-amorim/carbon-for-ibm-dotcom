3ce017595a4321bfa40df84493c3ecc52841d0f0
bruno-amorim <bruno.amorim07@hotmail.com>
2026-03-25 16:33:21 -0300

    fix(shopping-cart): display issue with new handleURL

--- packages/web-components/src/components/masthead/masthead-cart.ts
+++ packages/web-components/src/components/masthead/masthead-cart.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2024
+ * Copyright IBM Corp. 2024, 2026
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -47,12 +47,72 @@ class C4DMastheadCart extends StableSelectorMixin(LitElement) {
 
   connectedCallback() {
     super.connectedCallback();
-    // Check the relevant cookie for whether the user has an active cart.
-    this.hasActiveCart = SAPCommerceAPI.hasActiveCart();
-    // Fetch the locale for the page.
+
+    // Initial check
+    this.updateCart();
+
+    // Fallback retry
+    this.checkCartWithRetry();
+
+    // Listen for cross-tab updates
+    window.addEventListener('storage', this.updateCart);
+
+    // Fetch locale
     LocaleAPI.getLocale().then((locale) => {
       this.locale = locale;
     });
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    window.removeEventListener('storage', this.updateCart);
+  }
+
+  /**
+   * Updates cart state
+   */
+  updateCart = () => {
+    const hasCart = SAPCommerceAPI.hasActiveCart();
+    this.hasActiveCart = hasCart;
+  };
+
+  /**
+   * Checks if the user has an active cart and retries a few times if the cart cookie is not available yet.
+   */
+  checkCartWithRetry(retries = 5, delay = 200) {
+    const check = () => {
+      const hasCart = SAPCommerceAPI.hasActiveCart();
+      console.log(hasCart, 'hasCart 123456');
+
+      if (hasCart) {
+        this.hasActiveCart = true;
+        return;
+      }
+
+      if (retries > 0) {
+        console.log('retries 123456');
+        retries--;
+        setTimeout(check, delay);
+      }
+    };
+
+    check();
+  }
+
+  /**
+   * Filter the correct checkout URL based on locale and country code.
+   */
+  handleURL(cc: string, lc: string) {
+    switch (cc) {
+      case 'uk':
+        return `/store/en/gb/checkout`;
+      case 'ae':
+        return `/store/en/ae/checkout`;
+      case 'sa':
+        return `/store/en/sa/checkout`;
+      default:
+        return `/store/${lc}/${cc}/checkout`;
+    }
   }
 
   updated(changedProperties) {
@@ -72,7 +132,7 @@ class C4DMastheadCart extends StableSelectorMixin(LitElement) {
     return html`
       <a
         part="cart-link"
-        href="/store/${lc}/${cc}/checkout"
+        href="${this.handleURL(cc, lc)}"
         class="${prefix}--header__menu-item ${prefix}--header__menu-title"
         aria-label="${linkLabel}"
         >${ShoppingCart20()}</a
